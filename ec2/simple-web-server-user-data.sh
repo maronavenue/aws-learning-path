55a28c493b7c537e886374cbe22c49ac3a1c8229
Maron Montano <maron.montano@gmail.com>
2020-12-09 18:38:53 +0800

    Update EC2 simple web server shell script to show AZ and format info better

--- ec2/simple-web-server-user-data.sh
+++ ec2/simple-web-server-user-data.sh
@@ -4,7 +4,8 @@
 # TODO: Copy as plaintext inside EC2 User Data text field (only presented in bash file for formatting purposes)
 
 yum update -y
-yum install -y httpd.x86_64
+yum install -y httpd
 systemctl start httpd.service
 systemctl enable httpd.service
-echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+EC2_AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+echo "<h2><pre>Hello World from $(hostname -f) in AZ: ${EC2_AZ}</pre></h2>" > /var/www/html/index.html
